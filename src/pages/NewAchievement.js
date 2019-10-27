d1ff821c2ea2398bf8239610a047c52f64fbc1a3
KevM7 <kev.moonesawmy@gmail.com>
2019-10-27 13:05:38 +0100

    feat (new achievement) : add new achievement form with API call

--- src/pages/NewAchievement.js
+++ src/pages/NewAchievement.js
@@ -9,38 +9,55 @@ import {
   Icon
 } from 'saagie-ui/react';
 
-const NewAchievement = () => (
-  <div className="sui-l-app-layout">
-    <div className="sui-l-app-layout__subapp">
-      <AppLayoutPage>
-        <PageHeader title="SaagieVements">
-          <Link to="/" className="sui-a-button as--primary">
-            Achievements
-          </Link>
-        </PageHeader>
-        <Formsy
-          onValidSubmit={data => {
-            console.log(data)
-            alert('Form Submitted')
-          }}
-        >
-          <FieldInput
-            name="goalField"
-            label="New achievement"
-            placeholder="This is a new achievement."
-            helper="Describe your new achievement."
-            required
-            validationErrors={{
-              isDefaultRequiredValue: 'A goal achievement description is required',
-            }}
-          />
-          <button type="submit" className="sui-a-button">
-            <Icon name="plus" />
-          </button>
-        </Formsy>
-      </AppLayoutPage>
-    </div>
-  </div>
-);
+import { addNewAchievementFromAPI } from '../services/AchievementsService';
 
-export default NewAchievement
+export default class NewAchievement extends React.Component {
+
+  /**
+   * API call to add a new achievement on submit form
+   */
+  handleFormSubmit = data => {
+    const goal = data.goalField
+    addNewAchievementFromAPI(goal)
+      .then((res) => {
+        if (res.status === 200) {
+          alert("Your achievement has been added")
+        } else {
+          alert("An error has occurred")
+        }
+      })
+  }
+
+  render() {
+    return (
+      <div className="sui-l-app-layout">
+        <div className="sui-l-app-layout__subapp">
+          <AppLayoutPage>
+            <PageHeader title="SaagieVements">
+              <Link to="/" className="sui-a-button as--primary">
+                Achievements
+              </Link>
+            </PageHeader>
+            <Formsy
+              onValidSubmit={this.handleFormSubmit}
+            >
+              <FieldInput
+                name="goalField"
+                label="New achievement"
+                placeholder="This is a new achievement."
+                helper="Describe your new achievement."
+                required
+                validationErrors={{
+                  isDefaultRequiredValue: 'An achievement description is required',
+                }}
+              />
+              <button type="submit" className="sui-a-button">
+                <Icon name="plus" />
+              </button>
+            </Formsy>
+          </AppLayoutPage>
+        </div>
+      </div>
+    )
+  }
+}
